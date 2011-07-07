6c02fb67f9f8429fcac43bbe3140b7c958a40024
Antti Peisa <antti.peisa@gmail.com>
2011-07-07 14:44:24 +0300

    new horizontal adminbar

--- AdminBar.js
+++ AdminBar.js
@@ -2,127 +2,110 @@
 jQuery(document).ready(function($) {
 
 	//prepare all js-enhanced functions
-	$('body').addClass('editbar-jsready');
-
-	//prepare variable shortcuts
-	var $a			= $('#adminbar'),
-		$m			= $a.find('.main'),
-		$s			= $('<div class="collapse-switch"></div>').appendTo($m),
-		$sc			= $a.find('ul.shortcuts:eq(0)'),
-		$scLinks	= $sc.find('a').not('.hide'),
-		$tooltip	= $a.find('.tooltip'),
-		$ml         = $('<div id="ab-modal" />'), //
-		$mlClose    = $('<div id="ab-close-modal"><span>X</span> close</div>'), //
-		$mlLink		= $a.find('.modal'), // Links that should open in modal
-		$mlBg		= $('<div id="ab-modal-bg" />'); // Modal bg
-
-	// state variables
-	var mlGenerated;
-
-	// Changes between shortcuts and "main" window
-	function flip(){
-		if($m.hasClass('collapsed')){
-			$m.animate({left: '0px'},250);
-			$sc.hide();
-		}else{
-			$m.animate({left: '-200px'},250, function(){$sc.show();});
-		}
-		$m.toggleClass('collapsed');
-	}
-
-	//CSS manipulation and FX/event handlers
-	//hide 'collapse' icon overlay and set hover
-	$s.hover(function(){$(this).animate({opacity: 1},150);},
-			function(){$(this).animate({opacity: 0},150);});
-
-	$s.click(function(){ flip(); });
-
-	$sc.find('.hide').click(function(e){
-		e.preventDefault();
-		$a.hide('fast');
+	$('body').addClass('adminbar-loaded');
+	
+	var $adminbar = $('#adminbar');
+	var $li = $adminbar.find('li');
+	var $links = $adminbar.find('a').not('admin');
+	var $modalLinks = $adminbar.find('a.modal');
+	var $browse = $adminbar.find('.browse a');
+	
+	
+	var $modal = $('<div id="ab-modal"></div>').data('active', 'browse');
+	
+	var clicked = false;
+	
+	$links.click(function(){
+		setActive($(this));
 	});
+	
+	
+	
+	$modalLinks.click(function(){
+		if(!clicked) {
+			$modal.prependTo('body');
+		};
 
-	$scLinks.hover(
-		// Hover on shortcut link
-		function(){
-			var $this = $(this);
-			var position = $this.position();
-			var html = $this.html();
-
-			$tooltip.show().css({
-				'top': (position.top + 55) + 'px'
-				})
-			.find('.content').html(html);
-		},
-		// Hover out from shortcut link
-		function(){
-			$tooltip.hide();
-		}
-	);
-
-
-	// Bind custom events for modal background (open, close & click)
-	$mlBg.
-	bind('open', function(e) {
-		var $this = $(this);
-		$ml.removeClass('ab-modal-closed').addClass('ab-modal-open');
-		$this.removeClass('ab-modal-bg-closed');
-		$this.css('height', $(document).height());
-	}).
-	bind('close', function(e) {
-		var $this = $(this);
-		$this.addClass('ab-modal-bg-closed');
-	}).
-	bind('click', function(e) {
-		var $this = $(this);
-		$this.trigger('close');
-	});
-
-	$mlClose.
-	bind('click', function(e) {
-		$mlBg.trigger('close');
-	});
-
-	// If there is pagesaved notification, hide it after a moment
-	$('#ab-pagesaved-cont').delay(2400).slideUp("slow");
-
-	// Click event for links that are supposed to open in modal
-	$mlLink.click(function(e) {
-		e.preventDefault();
-		$(document).scrollTop(0);
-
-		if (!$m.hasClass('collapsed')) {
-			collapse();
-		}
-
-		// If modal has been generated at least once, but we now click different
-		// action than before, then we will remove current iFrame and re-create it later
-		if (mlGenerated && $ml.data('activeState') != e.target.className) {
-			mlGenerated = false;
-			$iframe.remove();
-		}
-
-		// If modal has not been generated before or state chanced in last if
-		if (!mlGenerated) {
-			mlGenerated = true;
-			$mlBg.prependTo('body');
-			$ml.appendTo($mlBg);
-
-			$mlClose.appendTo($ml);
-
-			// We save the class of the link as the modal active state (modal edit or modal add)
-			$ml.data('activeState', e.target.className);
-
-			// Create iFrame
+		if ($adminbar.data('active') == $(this).attr('class')) {
+			slideUp(true);
+		} else {
+			
+			$modal.addClass('loading').find('iframe').remove();
+			
+			
+			slideDown($(this));
+			// We "preload" this so it should be shown pretty fast after animation
 			$iframe = $('<iframe name="ab_modal_iframe" id="ab_modal_iframe" frameborder="0" src="'+ $(this).attr('href') +'"></iframe>')
 			.css('width', '100%')
-			.css('height', $(window).height() - 120)
-			.appendTo($ml);
+			.css('height', 0)
+			.appendTo($modal);
+		
+			
+			$adminbar.data('active', $(this).attr('class'));
+		};
+		
 
-			$mlBg.trigger('open');
-		} else {
-			$mlBg.trigger('open');
+		return false;
+	});
+	
+	$browse.click(function(){
+		slideUp(true);
+		return false;
+	});
+	
+	function setActive(link) {
+		$li.removeClass('active');
+		link.parent().addClass('active');
+	}
+	
+	function slideDown(link) {
+		
+		modalHeight = $(window).height() - 60; 
+		$modal.animate({
+			height: modalHeight + 'px'
+		},
+		
+		// Animation time
+		200,
+		
+		// After slide is done
+		function(){
+			$iframe.hide().css('height', $modal.height()).fadeIn("fast");
+		});
+	};
+	
+	function slideUp(clean) {
+		if (clean) {
+			$adminbar.data('active', 'browse');
+			$modal.find('iframe').remove();
+			setActive($browse);
 		}
-  })
+		$adminbar.data('active', 'browse');
+		$modal.removeClass('loading').stop().animate({
+			height: '0px'
+		}, 100, function(){
+			$modal.addClass('loading');
+		});
+		
+	};
+	
+	$(window).resize(function() {
+		if ($modal.height() > 1) {
+			modalHeight = $(window).height() - 60; 
+			$modal.stop().animate({
+				height: modalHeight + 'px'
+			},
+			
+			// Animation time
+			100, function(){
+				$iframe.css('height', $modal.height());
+			});
+		}
+		
+	});
+	
+	$("#ab-pagesaved").delay(3000).slideUp("fast");
+	
+	
 
 });
